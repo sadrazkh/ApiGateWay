44dfda0f2723955916d0396dd936c8ad2267dda3
sadra zadeh khameneh <sadra.z.kh@gmail.com>
2021-12-26 18:01:00 +0330

    Config Ocelot And Customize in run time

---
+++ ApiGateWay/Controllers/ServiceRegistrationController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NuGet.Packaging;
+
+namespace ApiGateWay.Controllers
+{
+    public class ServiceRegistrationController : Controller
+    {
+        // GET: ServiceRegistrationController
+        public async Task<ActionResult> Index()
+        {
+            using StreamReader r = new StreamReader("appsettings.json");
+            var json = r.ReadToEnd();
+            var items = JsonConvert.DeserializeObject<Rootobject>(json);
+            r.Close();
+            items.Routes.Add(new Route()
+            {
+                UpstreamPathTemplate = "/prr",
+                DownstreamScheme = "https",
+                DownstreamPathTemplate = "/User",
+                DownstreamHostAndPorts = new List<Downstreamhostandport>()
+                    {new Downstreamhostandport() {Host = "localhost", Port = 7048}},
+                UpstreamHttpMethod = new List<string>() {"Get"},
+            });
+            
+            //using StreamWriter w = new StreamWriter("appsettings.json");
+            //w.Write(items);
+            var dsd = items;
+            var text = JsonConvert.SerializeObject(items);
+            await System.IO.File.WriteAllTextAsync("appsettings.json", text);
+            return View();
+        }
+
+        // GET: ServiceRegistrationController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ServiceRegistrationController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ServiceRegistrationController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ServiceRegistrationController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ServiceRegistrationController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ServiceRegistrationController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ServiceRegistrationController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
+
+
+public class Rootobject
+{
+    public List<Route> Routes { get; set; }
+    public Globalconfiguration GlobalConfiguration { get; set; }
+}
+
+public class Globalconfiguration
+{
+    public string BaseUrl { get; set; }
+}
+
+public class Route
+{
+    public string DownstreamPathTemplate { get; set; }
+    public string DownstreamScheme { get; set; }
+    public List<Downstreamhostandport> DownstreamHostAndPorts { get; set; }
+    public string UpstreamPathTemplate { get; set; }
+    public List<string> UpstreamHttpMethod { get; set; }
+}
+
+public class Downstreamhostandport
+{
+    public string Host { get; set; }
+    public int Port { get; set; }
+}
--- ApiGateWay/Program.cs
+++ ApiGateWay/Program.cs
@@ -1,11 +1,45 @@
+using ApiGateWay.Controllers;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+//builder.WebHost.UseContentRoot(Directory.GetCurrentDirectory());
+//builder.WebHost.ConfigureKestrel(serverOption =>
+//    {
+//        serverOption.AddServerHeader = false;
+//    }).UseIISIntegration()
+//    .ConfigureAppConfiguration((context, configurationBuilder) =>
+//    {
+//        var env = context.HostingEnvironment;
+//        configurationBuilder
+//            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+//            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: false, reloadOnChange: true)
+//            .AddEnvironmentVariables();
+//        if (context.HostingEnvironment.IsDevelopment() || context.HostingEnvironment.EnvironmentName == "local")
+//        {
+//        }
+//        else
+//        {
+//        }
+
+//        configurationBuilder.Build();
+//    });
 
 // Add services to the container.
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    //.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
 
-builder.Services.AddControllers();
+
+builder.Services.AddOcelot(configuration: builder.Configuration);
+
+builder.Services.AddControllersWithViews();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -19,7 +53,24 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    //endpoints.MapControllerRoute(
+    //    name: "Account",
+    //    pattern:"{{area:exists}/{action = Index}/{id ?}}"
+    //);
+    endpoints.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+});
 
 app.MapControllers();
+app.UseOcelot().Wait();
 
 app.Run();
