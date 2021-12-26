ae66442dbc393a2d8d3c6fe7d2d906dcde7304f1
sadra zadeh khameneh <sadra.z.kh@gmail.com>
2021-12-26 15:35:04 +0330

    Add Two Test Service

---
+++ ProfileService/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProfileService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private static readonly string[] names = new[]
+        {
+        "Sadra", "Ali", "Mohammad", "Ehsan", "Younes", "AmirReza", "Amir", "Reza", "Hossein", "Hasan"};
+
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<User> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new User
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Name = names[Random.Shared.Next(names.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
